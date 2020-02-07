f07067feb3440c168b22ea5e1aa9f15b86971190
Yosuke Onoue <onoue@likr-lab.com>
2020-02-07 09:26:30 +0900

    Updates

---
+++ precache-manifest.460fcf63d7e2e75b5b5e272251c41499.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1318460551c248738e4e93c9ab518e4",
+    "url": "/kadai/index.html"
+  },
+  {
+    "revision": "b0b600e9900ebf0d623b",
+    "url": "/kadai/static/js/2.6a6b2c6c.chunk.js"
+  },
+  {
+    "revision": "a0a3063ed0e7e344881a79d4bea9f313",
+    "url": "/kadai/static/js/2.6a6b2c6c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c1fc54c5eb80e9e9aa7",
+    "url": "/kadai/static/js/main.d29178af.chunk.js"
+  },
+  {
+    "revision": "cc94455cc5e357f15836",
+    "url": "/kadai/static/js/runtime-main.028294b5.js"
+  }
+]);
